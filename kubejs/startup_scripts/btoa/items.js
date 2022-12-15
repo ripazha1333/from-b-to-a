22ea13714576ac8645d1286000b37f9bf77c8885
Ripazha1333 <nhada1333@gmail.com>
2022-12-14 22:52:59 -0600

    idk deleted unnecesary resources, added midgame items magic branch

---
+++ kubejs/startup_scripts/btoa/items.js
@@ -0,0 +1,8 @@
+onEvent('item.registry', event => {
+  event.create('b2a:bamboo_string')
+  event.create('b2a:bamboo_string_singularity')
+  event.create('b2a:compressed_stone')
+  event.create('b2a:stone_singularity')
+  event.create('b2a:nether_alloy')
+  event.create('b2a:nether_alloy_singularity')
+})
